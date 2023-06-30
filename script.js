57bb0815baa365770ac68767ab313e1b8d3a0b31
BohoBabe <bohobabe@BohoBabes-MBP.attlocal.net>
2023-06-30 09:50:59 -0700

    fixed bugs in phase 1

--- script.js
+++ script.js
@@ -6,24 +6,57 @@
 var upperCase = 'ABCDEFRHIJKLMNOPQRSTUVWXYZ'
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
 var numeric = '0123456789'
-var specialChar = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+var specialChar = "\"\ !#$%&'()*+,-./:;<=>?@[]^_`{|}~ "
 
 // This variable declaration references the HTML element of "generate" //
 var generateBtn = document.querySelector("#generate");
 
+// Phase One - gathering info, asking the user
 generatePassword = () => {
-  var passwordText = [];
-
+  var passwordResult = [];
   var length = prompt("Choose a password length between 8 and 128 characters")
-  var legnthParse = parseFloat(length);
-  var confirmSpecial = confirm("Would you like to use uppercase characters?")
-  var lowerCase = confirm("Would you like to use lowercase characters?")
-  var numeric = confirm("Would you like to use numbers?")
-  var specialChar = confirm ("Would you like to include special characters?")
+  
+  if (length < 8 || length > 128) {
+    alert("Your password must be between 8 and 128 characters")
+  } else {
+    var upperCasePrompt = confirm("Would you like to use uppercase characters")
+    var lowerCasePrompt = confirm("Would you like to use lowercase characters?")
+    var numericPrompt = confirm("Would you like to use numbers?")
+    var specialCharPrompt = confirm("Would you like to include special characters?")
+  }
+  
+  // if (length < 8 && length > 128) {
+  // } else {
+  //   alert("Your password must be between 8 and 128 characters")
+  // }
+
+// All are true or false values and since they are true by default they don't need the == //
+  if (upperCasePrompt) {
+    for (var i = 0; i <= upperCase.length; ++i) {
+      passwordText.push((Math.floor(Math.random() * password.Text.upperCase)))
+    }
+  }
+
+  if (lowerCasePrompt) {
+    for (var i = 0; i <= lowerCase.length; ++i) {
+      passwordText.push((Math.floor(Math.random() * password.Text.lowerCase)))
+    }
+  }
+
+  if (numericPrompt) {
+    for (var i = 0; i < passwordResult.length; ++i) {
+      passwordText.push((Math.floor(Math.random() * passwordText.numeric)))
+    }
+  }
+
+  if (specialCharPrompt) {
+    for (var i = 0; i < passwordResult.length; ++i) {
+      passwordText.push((Math.floor(Math.random() * passwordText.legth)))
+    }
+  }
+
 }
-
-
-
+// Phase 2 - building the password //
 
 // Write password to the #password input
 function writePassword() {
@@ -34,10 +67,13 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
 
 }
+
+
 
 // Pseudo Code
 // The user requests to generate a randome password
